be78acef274374ef77b9e81c44dde01f68305d7b
Brent Simpson <brent.simpson88@yahoo.com>
2022-03-31 22:07:25 -0400

    adding a form page and fixed navbar issue

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".navLinks");
 
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
+  navLinks.classList.toggle("is-active");
 });
