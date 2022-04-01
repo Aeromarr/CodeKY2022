9764e390b7800cd36e906268108b0f5c0f4f22f7
Brent Simpson <brent.simpson88@yahoo.com>
2022-04-01 00:58:33 -0400

    added a form and tried to add validation

--- index.js
+++ index.js
@@ -5,3 +5,53 @@ hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
   navLinks.classList.toggle("is-active");
 });
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = elemnt.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const validateInputs = () => {
+  const emailValue = email.value.trim();
+
+  if (Name === "") {
+    setError(Name, "Name is required");
+  } else {
+    setSuccess(Name);
+  }
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Provide a valid email address");
+  } else {
+    setSuccess(email);
+  }
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+};
